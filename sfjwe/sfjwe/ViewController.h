9eca91629f2890bf01178f971fd94d9dacbe9774
liguang <liguang@qq.com>
2015-12-30 16:01:04 +0800

    水电费我

---
+++ sfjwe/sfjwe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sfjwe
+//
+//  Created by liguang on 15/12/30.
+//  Copyright © 2015年 liguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sfjwe/sfjweTests/sfjweTests.m
@@ -0,0 +1,39 @@
+//
+//  sfjweTests.m
+//  sfjweTests
+//
+//  Created by liguang on 15/12/30.
+//  Copyright © 2015年 liguang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface sfjweTests : XCTestCase
+
+@end
+
+@implementation sfjweTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
